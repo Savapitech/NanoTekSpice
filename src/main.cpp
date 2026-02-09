3a1ee16dd4392c14d70441d1d37cba04ddd91b17
savalet <savinien.petitjean@gmail.com>
2026-02-09 10:37:48 +0000

    Fix main return value

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,14 @@
 #include <iostream>
 
 
-#define SUCCESSS 1;
+#define SUCCESSS 0;
 #define FAILURE 84;
 
 int main(int ac, char **av) {
+  if (ac != 2) {
+    std::cerr << "Usage: ./nanotekspice file.nts" << std::endl;
+    return FAILURE;
+  }
+
   return SUCCESSS;
 }
